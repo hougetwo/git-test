bd87e72cf49b524cdf6574371bea1de32aa972a4
zhangsan <zhangsan@itcast.cn>
2019-04-02 16:34:07 +0800

    第一次添加到本地仓库

---
+++ src/cn/itcast/git.java
@@ -0,0 +1,11 @@
+package cn.itcast;
+
+public class git {
+
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaaaaaaaaaaaa");
+        System.out.println("bbbbbbbbbbbbbbbbbb");
+        System.out.println("cccccccccccccccccc");
+
+    }
+}
